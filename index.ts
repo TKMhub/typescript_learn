c19ea40461be9556cb4a370a19597bf2ab85ad2e
TKMhub <111867865+TKMhub@users.noreply.github.com>
2024-11-28 23:40:35 +0900

    1128

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 let hasValue: boolean = true;
 let count: number = 10;
+let float: number = 3.14;
+let negative: number = -3.14;
+let double: string = "hello";
+let single: string = "hello";
+let back: string = `hello`;
