985e3c28f416deef16e6767d6534e96cd8e3f844
TKMhub <111867865+TKMhub@users.noreply.github.com>
2024-12-08 22:58:27 +0900

    1208_tsconfug_lib

--- tmp/compiler.ts
+++ tmp/compiler.ts
@@ -1,7 +1,10 @@
-console.log("compile!");
+// console.log("compile!");
 
 //複数のコンパイルが可能
 // tsc TS_compiler.ts compiler.ts
 
 // tsc --initでconfigが作成される　→ 以降は、tscで全てのtsファイルを一気にコンパイルすることができる
 // tsconfig.json;
+
+let hello = "hello";
+console.log(hello.toUpperCase());
