46b59a7ca468db7ab33e9d6e742dda5c9220c0bb
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-01-03 09:00:45 +0900

    0103_app

---
+++ section9_appCreate/food-app.ts
@@ -0,0 +1,19 @@
+class Score {}
+
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener("click", this.clickEventHandler.bind(this));
+  }
+  clickEventHandler() {
+    this.element.classList.toggle("food--active");
+  }
+}
+
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>(".food");
+  constructor() {
+    this.elements.forEach((element) => {});
+  }
+}
+
+const foods = new Foods();
