2628ac19764f30edd7d75dfbdd66cfd416138ec1
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-01-08 23:25:51 +0900

    0108_inport_export

--- section10_module_webpack/foods.ts
+++ section10_module_webpack/foods.ts
@@ -1,7 +1,18 @@
+//⭐️ importは合計5つ書き方がある
+//①名前付きインポート
 import { Foodsable } from "./interfaces";
+//②名前を変えることができる
+import { Foodsable as FoodLostable } from "./interfaces";
+//③*で全部を表現
+import * as interfaces from "./interfaces";
+//④デフォルトexport
+import asdfghDefault from "./interfaces";
 import { Food2 } from "./food";
 
-export class Foods2 implements Foodsable {
+// export class Foods2 implements Foodsable {
+// export class Foods2 implements FoodLostable {
+// export class Foods2 implements interfaces.Foodsable {
+export class Foods2 implements asdfghDefault {
   elements = document.querySelectorAll<HTMLDivElement>(".food");
   private _activeElements: HTMLDivElement[] = [];
   private _activeElementsStore: number[] = [];
--- section10_module_webpack/interfaces.ts
+++ section10_module_webpack/interfaces.ts
@@ -1,3 +1,16 @@
+//⭐️exportには2種類の書き方がある
+//①名前付きexport（通常のexport）
+export interface Foodsable {
+  elements: NodeListOf<HTMLDivElement>;
+  readonly activeElements: HTMLDivElement[];
+  readonly activeElementsScore: number[];
+}
+//②デフォルトexport
+export default interface FoodsableDefault {
+  elements: NodeListOf<HTMLDivElement>;
+  readonly activeElements: HTMLDivElement[];
+  readonly activeElementsScore: number[];
+}
 export interface Scoreable {
   readonly totalScore: number;
   render(): void;
@@ -5,9 +18,4 @@ export interface Scoreable {
 export interface Foodable {
   element: HTMLDivElement;
   clickEventHandler(): void;
-}
-export interface Foodsable {
-  elements: NodeListOf<HTMLDivElement>;
-  readonly activeElements: HTMLDivElement[];
-  readonly activeElementsScore: number[];
 }
