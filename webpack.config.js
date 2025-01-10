2f9e9e15ab96a2c6f034a27628812f52b2bc7176
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-01-10 22:04:46 +0900

    0110_tsLoader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./dist/section10_module_webpack/main.js",
+  entry: "./src/section10_module_webpack/main.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
