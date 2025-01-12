fac52c36e77d36e24e89445f457301754f5a0c6e
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-01-12 15:42:05 +0900

    0112_react_ts

--- vite-react-learn/src/ChildComp.tsx
+++ vite-react-learn/src/ChildComp.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { useState, ReactNode, MouseEvent } from "react";
 //⭐️<>タグ要素でなければならない
 // import { ReactElement } from "react";
 
@@ -8,11 +8,18 @@ interface ChildCompProps {
 }
 
 function ChildComp({ message, children }: ChildCompProps) {
+  const [count, setCoount] = useState<number>(0);
+  function updateCount(event: MouseEvent) {
+    setCoount(event.clientY);
+  }
   return (
     <div>
       <h2>Child</h2>
       <p>{message}</p>
       <div>{children}</div>
+      <p>counst: {count}</p>
+      <button onClick={() => setCoount(count + 1)}>+1</button>
+      <button onClick={(event) => updateCount(event)}>+1</button>
     </div>
   );
 }
